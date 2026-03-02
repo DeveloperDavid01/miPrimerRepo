db038289082db5f25b2d50bae501e1f7915e12ea
DeveloperDavid01 <davidfrencisco05@gmail.com>
2026-03-02 13:02:55 -0500

    Se elimino una llave, que anteriormente se había creado para hacer una prueba.

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key = "sk_test_51M9n7sH8qj2mLh5a3b6c7d8e9f0g1h2i3j4k5l6";
+
